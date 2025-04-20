55a6e1743349bd0d5ca2d0816749e173b8fafd61
nick-kuh <nickkuhnast@gmail.com>
2025-04-20 10:52:09 -0300

    correção settings3

--- jid_cantina/settings.py
+++ jid_cantina/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 
 import dj_database_url
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
