c5e047a9705f3a0abd43ea82f65aafa3aea786ec
nick-kuh <nickkuhnast@gmail.com>
2025-04-20 11:15:07 -0300

    correção settings6

--- jid_cantina/settings.py
+++ jid_cantina/settings.py
@@ -76,20 +76,20 @@ WSGI_APPLICATION = 'jid_cantina.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME','Cantina JID'),  
-        'USER': os.getenv('DB_USER','postgres'),  
-        'PASSWORD': os.getenv('DB_PASSWORD','databaseNick'), 
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
-}
+# DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': BASE_DIR / 'db.sqlite3',
+#     # }
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME','Cantina JID'),  
+#         'USER': os.getenv('DB_USER','postgres'),  
+#         'PASSWORD': os.getenv('DB_PASSWORD','databaseNick'), 
+#         'HOST': os.getenv('DB_HOST', 'localhost'),
+#         'PORT': os.getenv('DB_PORT', '5432'),
+#     }
+# }
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
