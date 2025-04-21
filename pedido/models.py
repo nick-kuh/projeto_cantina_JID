c27a16ea6d06068646d4361f93bded146a8d6793
nick-kuh <nickkuhnast@gmail.com>
2025-04-21 16:39:51 -0300

    criação superuser6

--- pedido/models.py
+++ pedido/models.py
@@ -92,4 +92,4 @@ class PedidoEntregue(models.Model):
         return f"{self.cliente.nome}"
     
 class Usuario(AbstractUser):
-    usuario = models.ManyToManyField("Pedido")
+    usuario = models.ManyToManyField("Cliente")
